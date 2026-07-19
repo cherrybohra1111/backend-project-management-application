5627ff43453a0adfe354d0982d24c85551192ce7
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-19 23:25:58 +0530

    chore: add dotenv configuration

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+let myport=process.env.PORT;
+
 console.log("Start of the backend project");
 console.log("Added nodemon");
+console.log("PORT : ",myport);
