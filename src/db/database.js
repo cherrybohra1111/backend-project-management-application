6341764759be4ed885b16ca657a153335aa53607
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-31 00:01:06 +0530

    feat: connect backend with mongodb

--- src/db/database.js
+++ src/db/database.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+console.log(mongoose.version);
 
 const connectDB = async () => {
     try{
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+import dns from "dns";
+
+dns.setServers([
+    "1.1.1.1",
+    "8.8.8.8"
+]);
+
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/database.js";
 
 dotenv.config({
     path: "./.env",
 });
 
+
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDB()
+    .then(()=>{
+        app.listen(port, ()=>{
+            console.log(`Example app listening on port http://localhost:${port}`);
+        });
+    })
+    .catch( (err) =>{
+        console.error("MongoDB connection error", err)
+        process.exit(1);
+    })
