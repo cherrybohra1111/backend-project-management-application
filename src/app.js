feec66a49eb99f9fcde344c73b32beaad750aa1d
cherrybohra1111 <cherrybohra@gmail.com>
2026-07-31 01:37:45 +0530

    feat: add health check API endpoint

--- src/app.js
+++ src/app.js
@@ -18,8 +18,13 @@ app.use(
     }),
 );
 
-app.get("/", (req, res) =>{
-    res.send("Hello World!");
+
+import healthCheckRouter from  "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck",healthCheckRouter);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to basecampy");
 });
 
 export default app;
