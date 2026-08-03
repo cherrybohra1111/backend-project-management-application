3371d727e434580456c21ca75f5727f939e21e2d
cherrybohra1111 <cherrybohra@gmail.com>
2026-08-04 01:26:12 +0530

    feat: add mail templates for verification and password reset

---
+++ src/utils/mail.js
@@ -0,0 +1,41 @@
+import Mailgen from "mailgen";
+
+const emailVerificationMailgenContent = (username ,verificationUrl) =>{
+    return {
+        body: {
+            name: username,
+            intro: "Welcome to our app! we're excited to have you on board.",
+            action: {
+                instructions: "To verify your email please click on the following button",
+                button : {
+                    color: ""#22BC66"",
+                    text: "Verify your email",
+                    link: verificationUrl,
+                },
+            },
+            outro :
+                "Need help, or have questions? Just reply to this email, we'd love to help.",
+        }
+    };
+};
+
+const forgotPasswordMailgenContent = (username ,passwordResetUrl) =>{
+    return {
+        body: {
+            name: username,
+            intro: "We got a request to reset the password of your account",
+            action: {
+                instructions: "To reset your password, please click on the following button",
+                button : {
+                    color: "#3869D4",
+                    text: "Reset password",
+                    link: passwordResetUrl,
+                },
+            },
+            outro :
+                "Need help, or have questions? Just reply to this email, we'd love to help.",
+        }
+    };
+};
+
+export {emailVerificationMailgenContent, forgotPasswordMailgenContent};
