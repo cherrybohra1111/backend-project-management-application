a46eb1a40a342cb9313a22d5ac0d060ebe431605
cherrybohra1111 <cherrybohra@gmail.com>
2026-09-02 19:53:40 +0530

    feat : add email verification

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -164,4 +164,43 @@ const getCurrentUser = asyncHandler (async (req, res) => {
              "Current user fetched successfully"));
 });
 
-export { registerUser, login, logoutUser };
+const verifyEmail = asyncHandler (async (req, res) => {
+    const {verificationToken} = req.params;
+
+    if (!verificationToken){
+        throw new ApiError(400, "Email verification token is missing")
+    }
+
+    let hashedToken = crypto
+        .createHash("sha256")
+        .update(verificationToken)
+        .digest("hex")
+
+    const user = await User.findOne({
+        emailVerificationToken: hashedToken,
+        emailVerificationExpiry: {$gt: Date.now()}
+    })
+
+    if (!user){
+        throw new ApiError(400, "Token is invalid or expired");
+    }
+
+    user.emailVerificationExpiry= undefined;
+    user.emailVerificationToken=undefined;
+
+    user.isEmailVerified = true;
+    await user.save({ validateBeforeSave : false })
+
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200,
+            {
+                isEmailVerified: true,
+            },
+            "Email is verified",
+        ),
+    );
+});
+
+export { registerUser, login, logoutUser, getCurrentUser, verifyEmail };
