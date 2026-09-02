991ecd1187986c672e23ba47d15b470cf0bb3dd8
cherrybohra1111 <cherrybohra@gmail.com>
2026-09-03 01:40:32 +0530

    feat: add forgot password request

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { User } from "../models/user.models.js";
-import { emailVerificationMailgenContent, sendEmail } from "../utils/mail.js"
+import { emailVerificationMailgenContent, forgotPasswordMailgenContent, sendEmail } from "../utils/mail.js"
 import jwt from "jsonwebtoken"
 
 
@@ -295,4 +295,43 @@ const refreshAccessToken = asyncHandler (async (req, res) => {
     }
 });
 
-export { registerUser, login, logoutUser, getCurrentUser, verifyEmail , resendEmailVerification};
+const forgotPasswordRequest = asyncHandler (async (req, res) =>{
+    const {email} = req.body
+    const user = await User.findOne({email})
+
+    if (!user){
+        throw new ApiError(404, "User does not exists", [])
+    }
+
+    const { unHashedToken, hashedToken, tokenExpiry } = 
+    user.generateTemporaryToken();
+
+    user.forgotPasswordToken = hashedToken
+    user.forgotPasswordExpiry = tokenExpiry
+
+    await user.save({validateBeforeSave: false})
+
+    await sendEmail(
+        {
+            email: user?.email,
+            subject: "Password reset request",
+            mailgenContent: forgotPasswordMailgenContent(
+                user.username,
+                `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}` ,
+            ),
+        }
+    );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Password reset email has been sent on your email id"
+            )
+        )
+
+});
+
+export { registerUser, login, logoutUser, getCurrentUser, verifyEmail , resendEmailVerification, refreshAccessToken, forgotPasswordRequest};
