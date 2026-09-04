ca5c707ee2106e3b275e12e93092bd00ce8656dd
cherrybohra1111 <cherrybohra@gmail.com>
2026-09-04 22:15:19 +0530

    feat: add task model

---
+++ src/models/task.models.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+import {AvailableTasKStatues, TaskStatusEnum} from "../utils/constants.js"
+
+const taskSchema = new Schema({
+    title : {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description : {
+        type: String,
+    },
+    project : {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    assignedTo : {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    assignedBy : {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    statues : {
+        type: String,
+        enum: AvailableTasKStatues,
+        default: TaskStatusEnum.TODO,
+    },
+    attachments: {
+        type : [{
+            url : String,
+            mimetype : String,
+            size : Number
+        }],
+        default: [],
+    }
+}, {timestamps: true});
+
+export const Task = mongoose.model("Task", taskSchema);
