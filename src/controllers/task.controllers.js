f02df0669a37c59a0db835a9881c523d7c82f5ab
cherrybohra1111 <cherrybohra@gmail.com>
2026-09-09 15:31:14 +0530

    feat : add create task controller

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -8,8 +8,6 @@ import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
-import { pipeline } from "nodemailer/lib/xoauth2/index.js"
-
 
 const getTasks = asyncHandler(async (req, res) => {
     const { projectId } = req.params;
@@ -28,6 +26,43 @@ const getTasks = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, tasks, "Tasks fetched successfully"));
 });
 
+const createTask = asyncHandler (async (req, res) => {
+    const { projectId } = req.params;
+    const { title , description, assignedTo, status } = req.body;
+    const project = await Project.findById(projectId);
+
+    if (!project){
+        throw new ApiError(404, "Project not found");
+    }
+
+    const files = req.files || [];
+
+    const attachments = files.map((file) => {
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.filename}`,
+            mimetype : file.mimetype,
+            size: file.size,
+        }
+    });
+
+    const task = await Task.create({
+        title,
+        description,
+        project: new mongoose.Types.ObjectId(projectId),
+        assignedTo: assignedTo
+            ? new mongoose.Types.ObjectId(assignedTo)
+            : undefined,
+        status,
+        assignedBy: new mongoose.Types.ObjectId(req.user._id),
+        attachments,
+    });
+    
+    return res
+        .status(201)
+        .json(new ApiResponse(201, task, "Task created successfully"));
+});
+
 export {
     getTasks,
+    createTask,
 }
